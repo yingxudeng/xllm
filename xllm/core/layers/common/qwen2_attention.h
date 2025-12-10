8759a5c68f7a790c5714b5f190cc0a67118f9aad
dengyingxu <dengyingxu1@jd.com>
2025-12-10 23:21:08 +0800

    feat: add wrapper for torch_npu layers.

--- xllm/core/layers/common/qwen2_attention.h
+++ xllm/core/layers/common/qwen2_attention.h
@@ -17,13 +17,6 @@ limitations under the License.
 
 #include <torch/torch.h>
 
-#if defined(USE_MLU)
-#include "../mlu/attention.h"
-#elif defined(USE_CUDA)
-#include "../cuda/attention.h"
-#elif defined(USE_ILU)
-#include "../ilu/attention.h"
-#endif
 #include "framework/kv_cache/kv_cache.h"
 #include "framework/model/model_args.h"
 #include "framework/parallel_state/parallel_args.h"
@@ -31,6 +24,7 @@ limitations under the License.
 #include "framework/state_dict/state_dict.h"
 #include "layers/common/rms_norm.h"
 #include "linear.h"
+#include "platform_attention.h"
 #include "rotary_embedding.h"
 
 namespace xllm {
